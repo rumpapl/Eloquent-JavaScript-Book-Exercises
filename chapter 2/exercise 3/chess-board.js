a47a9c5842620b730557fa02c2884bd2b1af6e76
rumpapl <rumpapaul0201@gmail.com>
2024-02-01 23:24:56 +0600

    another solution

--- chapter 2/exercise 3/chess-board.js
+++ chapter 2/exercise 3/chess-board.js
@@ -36,17 +36,51 @@
 //   console.log(str);
 // }
 
-console.log("----solution three----");
-let size = 30;
+// console.log("----solution three----");
+// let size = 30;
+// let str = "";
+// for (let row = 0; row < size; row++) {
+//   for (let col = 0; col < size; col++) {
+//     if ((row + col) % 2 === 0) {
+//       str += " ";
+//     } else {
+//       str += "#";
+//     }
+//   }
+//   str += "\n";
+// }
+// console.log(str);
+
+console.log("----solution four----");
+const size = 8;
 let str = "";
-for (let row = 0; row < size; row++) {
-  for (let col = 0; col < size; col++) {
-    if ((row + col) % 2 === 0) {
-      str += " ";
+if (size % 2 === 0) {
+  evenRow = "";
+  oddRow = "";
+  for (let index = 0; index < size; index++) {
+    if (index % 2 === 0) {
+      evenRow += " ";
+      oddRow += "#";
     } else {
-      str += "#";
+      evenRow += "#";
+      oddRow += " ";
     }
   }
-  str += "\n";
+
+  for (let count = 0; count < size; count++) {
+    if (count % 2 === 0) {
+      str += evenRow;
+    } else {
+      str += oddRow;
+    }
+    str += "\n";
+  }
+} else {
+  for (let index = 0; index < size * size; index++) {
+    if (index % 2 === 0) str += " ";
+    else str += "#";
+    if ((index + 1) % size == 0) str += "\n";
+  }
 }
+
 console.log(str);
